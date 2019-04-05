0515988dfe4b111eda570b5b1fae51fc33869311
AmeliaShields <the.amelia.s@gmail.com>
2019-04-05 11:37:39 -0700

    Added code

---
+++ Assets/Scripts/CardCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CardCollection : MonoBehaviour
+{
+
+	private String _label;
+	private Stack<Card> _cards;
+
+	public CardCollection(String label)
+	{
+		this._cards = new Stack<Card>();
+		this._label = label;
+	}
+
+	public void addCard(Card card)
+	{
+		_cards.Push(card);
+	}
+
+	public void removeCard()
+	{
+		_cards.Pop();
+	}
+
+	public Card GetCard(int i)
+	{
+		return _cards.Peek();
+	}
+}
