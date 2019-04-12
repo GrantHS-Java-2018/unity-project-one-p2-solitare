a3d148e9017445f9fbb3edb314f85b5c9387f764
AmeliaShields <the.amelia.s@gmail.com>
2019-04-12 11:15:53 -0700

    Edited code

--- Assets/Scripts/CardDeck.cs
+++ Assets/Scripts/CardDeck.cs
@@ -4,57 +4,8 @@ using UnityEngine;
 
 public class CardDeck : CardCollection
 {
-    private Card card = new Card();
    
-    private Card.Suit[] intToSuit = {
-        Card.Suit.Diamond,
-        Card.Suit.Club,
-        Card.Suit.Heart,
-        Card.Suit.Spade
-    };
-
-    private Card.Rank[] intToRank = {
-        Card.Rank.Two,
-        Card.Rank.Three,
-        Card.Rank.Four,
-        Card.Rank.Five,
-        Card.Rank.Six,
-        Card.Rank.Seven,
-        Card.Rank.Eight,
-        Card.Rank.Nine,
-        Card.Rank.Ten,
-        Card.Rank.Jack,
-        Card.Rank.Queen,
-        Card.Rank.King,
-        Card.Rank.Ace
-    };
-
-    private Queue<Card> deckOfCards = new Queue();// Contains all 52 cards
-
-    public CardDeck()    // Constructor
-    {
-        for ( int suit = 0; suit < 4; suit++ ) {
-            for ( int rank = 0; rank < 13; rank++ ) {
-                deckOfCards.Enqueue(new Card(intToSuit[suit], intToRank[rank]));
-            }
-        }
-        shuffleDeck();
-       
-
-    }
-
-    public void shuffleDeck()
-    {
-        deckOfCards.
-        Collections.shuffle(deckOfCards);
-    }
-
-    public Card dealCard()
-    {
-        Card topCard = deckOfCards.get(0);
-        deckOfCards.remove(0);
-        return topCard;
-    }
+   
 }
 
 
