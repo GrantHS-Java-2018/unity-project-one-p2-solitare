701e46bfae1019641c6f20187ac9e3f8fcfd6cd9
AmeliaShields <the.amelia.s@gmail.com>
2019-04-12 10:32:59 -0700

    Comment

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 
 public class Rules : MonoBehaviour {
-
+	/*Should have most of the game play functions, like maybe moving the cards when someone
+	 clicks on them and making sure that cards only go to valid positions */ 
+	
 	
 		
 	
