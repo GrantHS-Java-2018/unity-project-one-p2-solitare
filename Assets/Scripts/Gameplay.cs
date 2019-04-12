a92bbb9ceac2ea1069b2eb255c06d84a2e75ccde
quinn <quinnopeters>
2019-04-12 11:23:42 -0700

    rendered all cards, moved them from prefabs

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -2,6 +2,14 @@
 
 public class Rules : MonoBehaviour {
 
+	/*shuffled deck
+	  cards go in correct places
+	  player can click on cards to move them 
+	  they can click the draw deck for a new card to be included
+	  they can move the cards to the ace pile
+	  player can quit or win
+	  card will flip
+	 */
 	
 		
 	
